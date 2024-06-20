f577af91453916f711d7a4d9e48a3eb1fa060eda
DiegoDom <dominguezdiex@gmail.com>
2024-06-20 11:06:48 -0600

    Counter functionality (shoppingcart)

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata: Metadata = {
+  title: "Shopping Cart page",
+  description: "This page makes a great example of cliente side components needs",
+};
+
 export default function CounterPage() {
   return (
-    <>
-      <h1>CounterPage</h1>
-    </>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Shopping Cart Products</span>
+      <CartCounter value={1} />
+    </div>
   );
 }
